c6cb7dccb09e4533ae44283a8f34c00b8896fde8
Jorge <jorgitoadame17@gmail.com>
2024-10-09 15:45:17 -0700

    init

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import ProfileInfo from "./Cards/ProfileInfo";
+import { useNavigate } from "react-router-dom";
+import SeachBar from "./SeachBar";
 
 const Navbar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const navigate = useNavigate();
+  const logout = () => {
+    navigate("/login");
+  };
+  const handleSearch = () => {};
+  const onClearSearch = () => {
+    setSearchQuery("");
+  };
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
-        <h2 className="text-xl font-medium text-black py-2">Notes</h2>
+      <h2 className="text-xl font-medium text-black py-2">Notes</h2>
+
+      <SeachBar
+        value={searchQuery}
+        onChange={({ target }) => {
+          setSearchQuery(target.value);
+        }}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch}
+      />
+
+      <ProfileInfo onLogout={logout} />
     </div>
-  )
+  );
 };
 
 export default Navbar;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
+import NoteCard from "../../components/Cards/NoteCard";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+
+      <div className="container mx-auto">
+        <NoteCard
+          title="Hi"
+          date="10/10/10"
+          content="hi"
+          tags="meeting"
+          isPinned={true}
+          onEdit={() => {}}
+          onDelete={() => {}}
+          onPinNote={() => {}}
+        />
+      </div>
+    </>
   );
 };
 
