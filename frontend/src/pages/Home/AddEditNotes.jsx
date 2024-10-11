29908417e0625c72944e9b953a2bd21aef8b3d5f
Jorge <jorgitoadame17@gmail.com>
2024-10-10 20:57:05 -0700

    commit

--- frontend/src/pages/Home/AddEditNotes.jsx
+++ frontend/src/pages/Home/AddEditNotes.jsx
@@ -1,31 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import TagInput from "../../components/Input/TagInput";
+import { MdClose } from "react-icons/md";
 
-const AddEditNotes = () => {
+const AddEditNotes = ({ noteData, type, onClose }) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [tags, setTags] = useState([]);
+
+  const [error, setError] = useState(null);
+
+  const addNewNote = async () => {};
+
+  const editNote = async () => {};
+
+  const handleAddNote = () => {
+    if (!title) {
+      setError("Please enter a title");
+      return;
+    }
+
+    if (!content) {
+      setError("Please enter the content of your note");
+      return;
+    }
+
+    setError("");
+
+    if (type === "edit") {
+      editNote();
+    } else {
+      addNewNote();
+    }
+  };
+
   return (
-    <div className="flex flex-col gap-2">
-      <label className="input-label">Title</label>
-      <input
-        type="text"
-        className="text-2xl text-slate-950 outline-none"
-        placeholder="The title for your note."
-      />
+    <div className="relative">
+      <button
+        className="w-10 h-10 rounded-full flex items-center justify-center absolute -top-3 -right-3 hover:bg-slate-50"
+        onClick={onClose}
+      >
+        <MdClose className="text-xl text-slate-400" />
+      </button>
 
-      <div className="flex flex-col gap-2 mt-4">
-        <label className="input-label">Content</label>
-        <textarea
+      <div className="flex flex-col gap-2">
+        <label className="input-label">Title</label>
+        <input
           type="text"
-          className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
-          placeholder="Content"
-          rows={10}
+          className="text-2xl text-slate-950 outline-none"
+          placeholder="The title for your note."
+          value={title}
+          onChange={({ target }) => setTitle(target.value)}
         />
-      </div>
 
-      <div className="mt-3">
-        <label className="input-label">Tags</label>
-        
-      </div>
+        <div className="flex flex-col gap-2 mt-4">
+          <label className="input-label">Content</label>
+          <textarea
+            type="text"
+            className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
+            placeholder="Content"
+            rows={10}
+            value={content}
+            onChange={({ target }) => setContent(target.value)}
+          />
+        </div>
 
-      <button className="btn-primary font-medium mt-5 p-3" onClick={() => {}}>Add</button>
+        <div className="mt-3">
+          <label className="input-label">Tags</label>
+          <TagInput tags={tags} setTags={setTags} />
+        </div>
+
+        {error && <p className="text-red-500 text-xs pt-4">{error}</p>}
+
+        <button
+          className="btn-primary font-medium mt-5 p-3"
+          onClick={handleAddNote}
+        >
+          Add
+        </button>
+      </div>
     </div>
   );
 };
